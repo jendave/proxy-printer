6b02a6209c99a477e4abfe9892e8bcc72e7b4235
David Hudson <jendave.hudson@gmail.com>
2012-10-20 08:13:18 -0700

    fix to batch

--- src/main/scripts/proxy-printer.bat
+++ src/main/scripts/proxy-printer.bat
@@ -78,7 +78,7 @@ goto endInit
 @REM Reaching here means variables are defined and arguments have been captured
 :endInit
 
-%JAVACMD% %JAVA_OPTS% %EXTRA_JVM_ARGUMENTS% -classpath %CLASSPATH_PREFIX%;%CLASSPATH% -Dapp.name="${app.name}" -Dapp.repo="%REPO%" -Dbasedir="%BASEDIR%" ${main.class} %CMD_LINE_ARGS%
+%JAVACMD% %JAVA_OPTS% %EXTRA_JVM_ARGUMENTS% -classpath %CLASSPATH_PREFIX%;%CLASSPATH% -Dapp.name="${app.name}" -Dapp.repo="%REPO%" -Dbasedir="%BASEDIR%" -jar ${project.artifactId}-${project.version}-jar-with-dependencies.jar %CMD_LINE_ARGS%
 if ERRORLEVEL 1 goto error
 goto end
 
